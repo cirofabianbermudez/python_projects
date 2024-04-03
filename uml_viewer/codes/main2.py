047af8ca27ba3a9e5c55a540840e6f5fda5e909a
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-04-03 19:24:31 +0200

    feat: structure of uml_viewer v0.1

---
+++ uml_viewer/codes/main2.py
@@ -0,0 +1,39 @@
+import yaml
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+def print_tree(map, prefix):
+
+    item_prefix = prefix + "├── "
+    sub_prefix  = prefix + "│   "
+
+    lenght = len(map.values());
+    # print(lenght)
+    counter = 1   
+
+    for key, value in map.items():
+
+        if counter == lenght:
+            sub_prefix  = prefix + "    "
+            item_prefix = prefix + "└── "
+            print(item_prefix + key)
+        else:
+            print(item_prefix + key)
+            counter = counter + 1
+
+        if value is not None:
+            print_tree(value, sub_prefix)
+
+if __name__ == "__main__":
+    file_path = 'uvm_struct.yml'
+    data = load_yaml(file_path);
+    print(data)
+
+
+
+
+
+
